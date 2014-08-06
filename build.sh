66ef5a2ab4d8afd84be2b27ce63553cee61a1cdf
Don Ky <don.d.ky@gmail.com>
2014-08-06 08:05:30 -0400

    Change ubuntu-64 to trusty, update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#rm -rf output-* packer_cache *.box
+NAME="trusty-64"
+
+rm -rf output-* packer_cache *.box
 packer build template.json
-vagrant box remove trusty-64
-vagrant box add trusty-64 packer_ubuntu-14_vmware.box
+vagrant box remove $NAME
+vagrant box add $NAME packer_$NAME_vmware.box
