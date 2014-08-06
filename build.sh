82bc15cbe0f1d973a2d1fd990a49db07bd502be7
Don Ky <don.d.ky@gmail.com>
2014-08-06 08:23:52 -0400

    surround  with  to expand variable

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 NAME="trusty-64"
 
-rm -rf output-* packer_cache *.box
-packer build template.json
-vagrant box remove $NAME
-vagrant box add $NAME packer_$NAME_vmware.box
+#rm -rf output-* packer_cache *.box
+#packer build template.json
+#vagrant box remove $NAME
+vagrant box add $NAME packer_"$NAME"_vmware.box
